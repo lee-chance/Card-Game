d892c732419001378cf72a03258cf27f087b0f63
Changsu Lee <729mail2@gmail.com>
2023-05-25 11:21:29 +0900

    Initial Commit

---
+++ CardGame/CardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardGameApp.swift
+//  CardGame
+//
+//  Created by 이창수 on 2023/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
